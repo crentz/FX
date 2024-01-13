d3beb2f2811adacbb9aea4567e41228a5bbe11e6
crentz <crentzc@gmail.com>
2024-01-13 18:14:16 +0000

    Update fxclo.sh

--- fxclo.sh
+++ fxclo.sh
@@ -7,20 +7,20 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Set the default ISO and system names
-DEFAULT_ISO_NAME="debian-live-clone.iso"
-DEFAULT_SYSTEM_NAME="debian-live-clone"
+DEFAULT_ISO_NAME="fluxuan-daedalus.iso"
+DEFAULT_SYSTEM_NAME="fluxuan"
 
 # Prompt user for ISO name
 ISO_NAME=$(whiptail --inputbox "Enter the name for the ISO file:" 8 60 "$DEFAULT_ISO_NAME" --title "ISO Name" 3>&1 1>&2 2>&3)
 
 # Prompt user for system name
-SYSTEM_NAME=$(whiptail --inputbox "Enter the name for the cloned Debian system:" 8 60 "$DEFAULT_SYSTEM_NAME" --title "System Name" 3>&1 1>&2 2>&3)
+SYSTEM_NAME=$(whiptail --inputbox "Enter system name:" 8 60 "$DEFAULT_SYSTEM_NAME" --title "System Name" 3>&1 1>&2 2>&3)
 
 # Set the ISO destination
-ISO_DESTINATION="/path/to/save/$ISO_NAME"
+ISO_DESTINATION="/home/$ISO_NAME"
 
 # Set the target directory for rsync
-RSYNC_TARGET_DIR="/tmp/debian-rsync-target"
+RSYNC_TARGET_DIR="/tmp/fluxuan-rsync-target"
 
 # Create a temporary directory for whiptail messages
 TMP_MSG_DIR=$(mktemp -d)
@@ -30,10 +30,10 @@ show_loading() {
   (
     echo "XXX"
     echo "10"
-    echo "Cloning Debian system..."
+    echo "Creating system..."
     sleep 1
 
-    # Use rsync to clone the Debian system
+    # Use rsync to clone the system
     rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} / "$RSYNC_TARGET_DIR"
 
     echo "XXX"
@@ -42,7 +42,7 @@ show_loading() {
     sleep 1
 
     # Create a directory for the live ISO
-    LIVE_ISO_DIR="/tmp/debian-live-iso"
+    LIVE_ISO_DIR="/tmp/fluxuan-live-iso"
     mkdir -p "$LIVE_ISO_DIR/live"
 
     echo "XXX"
